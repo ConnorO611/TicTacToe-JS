7717ea246ac89abf9ea10ab393f0ae5bdb7417dc
ConnorO611 <connor@ollenberger.com>
2025-10-25 17:44:53 -0700

    Updated Code in JS file
    Now draws the grid for Tic Tac Toe.

--- game.js
+++ game.js
@@ -1,15 +1,45 @@
 //Get the canvas from html
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-//Track Key States
-const keys={}
-window.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
-window.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
+//Get canvas size
+const width = canvas.width;
+const height = canvas.height;
+
+function drawBoard() {
+    //Vertical Line 1
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.moveTo((width * 1/3), 0);
+    ctx.lineTo((width * 1/3), height);
+    ctx.stroke();
+    //Vertical Line 2
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.moveTo((width * 2/3), 0);
+    ctx.lineTo((width * 2/3), height);
+    ctx.stroke();
+    //Horizontal Line 1
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.moveTo(0, (height * 1/3));
+    ctx.lineTo(width, (height * 1/3));
+    ctx.stroke();
+    //Horizontal Line 2
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.moveTo(0, (height * 2/3));
+    ctx.lineTo(width, (height * 2/3));
+    ctx.stroke();
+}
 //Draw on canvas
 function draw() {
-
+    ctx.clearRect(0, 0, width, height);
+    drawBoard();
 }
 //Gameloop
 function gameLoop() {
     draw();
+    requestAnimationFrame(gameLoop);
 }
+
+gameLoop();
